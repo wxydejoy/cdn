2d5a1010c962b2fce2d2b67d9001f056e3ec38cf
weiekko <81625961+wxydejoy@users.noreply.github.com>
2024-01-10 01:29:33 +0000

    ces

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cdn
 借助npm 又拍云 实现简易cdn
+
+http://h.undf.top:5244/d/upyun/Power/weekly/02.2024.01.08.md
+
+https://cdn.undf.top/weibo/output/tweets.json
---
+++ weekly/main.py
@@ -0,0 +1,27 @@
+
+
+
+import requests
+import os
+import datetime
+
+# datetime.datetime.now().strftime("%W.%Y.%m.%d")
+
+# 判断今天是周几
+if datetime.datetime.now().weekday() == 0:
+    # print("今天是周一")
+    datestr = datetime.datetime.now().strftime("%W.%Y.%m.%d") + ".md"
+else:
+    # print(datetime.datetime.now().weekday())
+    # print("今天不是周一")
+    datestr = (datetime.datetime.now() - datetime.timedelta(days=datetime.datetime.now().weekday())).strftime("%W.%Y.%m.%d") + ".md"
+
+
+
+# 获取环境变量 URL + WW.YYYY.MM.DD
+url = os.environ["URL"]  + datestr
+
+
+# 发送请求 并保存为md文件
+with open("weekly.md", "w", encoding="utf-8") as f:
+    f.write(requests.get(url).text)
